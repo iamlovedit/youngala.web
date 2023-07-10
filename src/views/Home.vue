4679f180c0da747aa04669a4869785dd59f2ebab
Young <iamlovedit@163.com>
2023-07-10 20:34:07 +0800

    update codes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,27 @@
 <template>
     <Content>
-        <div class="homeContainer">
+        <div ref="webgl" class="homeContainer">
 
         </div>
     </Content>
 </template>
 
 <script setup lang="ts">
-
+import { ref, onMounted } from 'vue'
 import Content from '@components/layout/content/Index.vue'
+import { renderer } from "@components/threejs/family";
+const webgl = ref<HTMLDivElement | null>(null)
 
+
+onMounted(() => {
+    webgl.value?.appendChild(renderer.domElement);
+    
+})
 </script>
 
 <style scoped lang="scss">
 .homeContainer {
     height: 100%;
+    width: 100%;
 }
 </style>
